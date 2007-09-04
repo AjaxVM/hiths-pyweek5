b4f5fe0c950c291054fb344d070937d3f818f5c4
roebros <roebros@cb0ddb06-e238-0410-b1ac-8f1d7c7f5fec>
2007-09-04 03:15:40 +0000

    added python config loader added lots of stuff to unit

--- pyweek/lib/modules/elements.py
+++ pyweek/lib/modules/elements.py
@@ -1,17 +1,36 @@
 from unit import *
+import pygame
+from pygame.locals import *
+import pyglibs
 
-def load_file(filename):
+def load_file(filename, iso_world):
     a=open(filename, 'rU').read()
-    races=[]
-    units=[]
-    houses=[]
-    players=[]
-    glyphs=[]
-    ground_glyphs=[]
+    races=[]#these are any races found in the file
+    units=[]#these are units from file
+    houses=[]#houses
+    players=[]#players
+    glyphs=[]#glyphs
+    ground_glyphs=[]#glyphs that are placed
+    glyph_spawn_rate=0#how quickly new glyphs are created
+    cities=[]#human cities
+    fortifications=[]#placed forts
+    monster_spawn_points=[]#this is where nasties apwn when oyu pick up a glyph!
+    map=None#any maps found in the file
+    ancient_temples=[]#this is where teh Rendth spawn from
+    campaigns=[]#any campaigns found in this file
+    terrains={}#the terrain types found in this file
     exec a
     return {'races':races,
             'units':units,
             'houses':houses,
             'players':players,
             'glyphs':glyphs,
-            'ground_glyphs':ground_glyphs}
+            'ground_glyphs':ground_glyphs,
+            'glyph_spawn_rate':glyph_spawn_rate,
+            'cities':cities,
+            'ms_points':monster_spawn_points,
+            'map':map,
+            'forts':fortifications,
+            'terrain':terrains,
+            'temples':ancient_temples,
+            'campaigns':campaigns}
