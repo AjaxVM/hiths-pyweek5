6c58794a5e4f34e23b40d31bc39f6b074c2a79d1
roebros <roebros@cb0ddb06-e238-0410-b1ac-8f1d7c7f5fec>
2007-09-05 05:58:19 +0000

    added state engine added OLDmain.py added config loader - it may have to be modified when campaigns/scenarios/players are added made config loader look in data for files first, then in data/campaigns

--- pyweek/lib/main.py
+++ pyweek/lib/main.py
@@ -8,10 +8,5 @@ import modules
 from modules import pyglibs, entities, elements, engine
 from modules.pyglibs import isometric, image, gui
 
-def spc_div(a, b):
-    if a and b:
-        return a/b
-    return 0
-
 def main():
-    engine=engine.Engine(state="game")
+    e=engine.Engine(state="game")
--- pyweek/lib/modules/elements.py
+++ pyweek/lib/modules/elements.py
@@ -1,45 +1,68 @@
-from entities import *
+import os
+
 import pygame
 from pygame.locals import *
+
+from entities import *
 import pyglibs
 from pyglibs import *
 from assets import *
 
 def path(*f):
-    return os.path.join('data', *f)
+    a=os.path.join('data', *f)
+    if os.path.isfile(a):return a
+    a=os.path.join('data', 'campaigns', *f)
+    if os.path.isfile(a):return a
+    raise AttributeError, "File '%s' not found"%str(f)
 
-def load_file(filename):
+def load_file(filename, other=None):
     a=open(filename, 'rU').read()
-    world=None#the isometric.World or decendant of one
-    camera_pos=[0,0]#the isometric.camera
-    races=[]#these are any races found in the file
-    players=[]#players
-    glyphs=[]#glyphs
-    ground_glyphs=[]#glyphs that are placed
-    glyph_spawn_rate=0#how quickly new glyphs are created
-    cities=[]#human cities
-    fortifications=[]#placed forts
-    monster_spawn_points=[]#this is where nasties apwn when oyu pick up a glyph!
-    maps={}#any maps found in the file
-    ancient_temples=[]#this is where teh Rendth spawn from
-    campaigns=[]#any campaigns found in this file
-    terrain={}#the terrain types found in this file
-    images={}
-    music={}
-    sfx={}
+    if other:
+        world=other['world']#the isometric.World or decendant of one
+        camera_pos=other['camera_pos']#the isometric.camera
+        races=other['races']#these are any races found in the file
+        glyphs=other['glyphs']#glyphs
+        ground_glyphs=other['ground_glyphs']#glyphs that are placed
+        glyph_spawn_rate=other['glyph_spawn_rate']#how quickly new glyphs are created
+        cities=other['cities']#human cities
+        fortifications=other['forts']#placed forts
+        monster_spawn_points=other['ms_points']#this is where nasties apwn when oyu pick up a glyph!
+        maps=other['maps']#any maps found in the file
+        ancient_temples=other['temples']#this is where teh Rendth spawn from
+        campaigns=other['campaigns']#any campaigns found in this file
+        terrain=other['terrain']#the terrain types found in this file
+        images=other['images']
+        music=other['music']
+        sfx=other['sfx']
+    else:
+        world=None#the isometric.World or decendant of one
+        camera_pos=[0,0]#the isometric.camera
+        races={}#these are any races found in the file
+        glyphs=[]#glyphs
+        ground_glyphs=[]#glyphs that are placed
+        glyph_spawn_rate=0#how quickly new glyphs are created
+        cities=[]#human cities
+        fortifications=[]#placed forts
+        monster_spawn_points=[]#this is where nasties apwn when oyu pick up a glyph!
+        maps={}#any maps found in the file
+        ancient_temples=[]#this is where teh Rendth spawn from
+        campaigns=[]#any campaigns found in this file
+        terrain={}#the terrain types found in this file
+        images={}
+        music={}
+        sfx={}
     exec a
     return {'world':world,
             'camera_pos':camera_pos,
             'races':races,
-            'players':players,
             'glyphs':glyphs,
             'ground_glyphs':ground_glyphs,
             'glyph_spawn_rate':glyph_spawn_rate,
             'cities':cities,
             'ms_points':monster_spawn_points,
-            'map':map,
+            'maps':maps,
             'forts':fortifications,
-            'terrain':terrains,
+            'terrain':terrain,
             'temples':ancient_temples,
             'campaigns':campaigns,
             'images':images,
